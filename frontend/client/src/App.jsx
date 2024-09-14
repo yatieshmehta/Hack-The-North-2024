39dd45ab15b01575a87a05ba409140f6536fbd1e
Andrew Chan <andrewhongchan@gmail.com>
2024-09-14 17:43:23 -0400

    Add more routes and navbar

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -2,14 +2,18 @@ import SignUpPage from "./components/SignUpForm/SignUpForm";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import CandidatesPage from "./components/CandidatesPage";
+import ProjectsPage from "./components/ProjectsPage";
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <Routes>
         <Route path="/login" element={<SignUpPage />}/>
         <Route path="/" element={<Home />}/>
-        
+        <Route path="/candidates" element={<CandidatesPage />}/>
+        <Route path="/projects" element={<ProjectsPage />}/>
       </Routes>
     </div>
   );
