7f09e473b607da9d5489bf230280fae7d3f13090
Andrew Chan <andrewhongchan@gmail.com>
2024-09-14 23:10:31 -0400

    Renaming

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import CandidatesPage from "./components/CandidatesPage";
+import CandidatesPage from "./components/ProjectApplicantsPage";
 import ProjectsPage from "./components/ProjectsPage";
 function App() {
   return (
